e7e435f5cf20aa0cc4a7a100707a5496496a8c4b
Eduardo Mata <79716108+kanazuko@users.noreply.github.com>
2023-07-14 15:51:59 -0600

    cambio de imagen y texto en header

--- src/App.js
+++ src/App.js
@@ -14,38 +14,7 @@ import Footer from './componentes/Footer.jsx';
 
 
 function App() {
-
-  // const [numClics,setNumClics] = useState(0);
-
-  // const manejarClic = () => {
-  //   setNumClics(numClics+1);
-  // };
-  // const reiniciarContador = () => {
-  //   setNumClics(0);
-  // };
-
   return (
-    //   <div className='freecodecamp-logo-contenedor'>
-    //     <img
-    //       className='freecodecamp-logo'
-    //       src={freeCodeCampLogo}
-    //       alt='Logo de freeCodeCamp' />
-    //   </div>
-    //   <div className='contenedor-principal'>
-    //     <Contador
-    //     numClics={numClics}
-    //     />
-    //     <Boton 
-    //       texto='Clic'
-    //       esBotonDeClic={true}
-    //       manejarClic={manejarClic}
-    //     />
-    //     <Boton 
-    //     texto='Reiniciar'
-    //     esBotonDeClic={false}
-    //     manejarClic={reiniciarContador}
-    //     />
-    //   </div>
     <div className="App">
       <Header />
       <Hero />
--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -2,7 +2,7 @@ import '../hojas-de-estilo/hero.css'
 function Header(){
     return(<section className="hero align-items-stretch">
     <div className="hero-principal d-flex flex-column justify-content-center align-items-center">
-          <img className="hero-imagen-desarrollador" src={require("../imagenes/qt=q_95.webp")} width="100" alt="GVA Logo" />
+          <img className="hero-imagen-desarrollador" src={require("../imagenes/retratoTitulo.png")} width="100" alt="GVA Logo" />
           <div className="row text-center align-items-center">
             <div className="columna col-12 col-md-8">
               <div className="hero-inferior">
@@ -10,8 +10,8 @@ function Header(){
               </div>
             </div>
           <div className="columna col-12 col-md-4">      
-          <h1>GVA ARQUITECTOSS!</h1>
-          <h2>La firma líder de arquitectura en Latinoamérica</h2>
+          <h1>Raul Eduardo Avila Mata</h1>
+          <h2>Desarrollador, ingeniero, gamer y apasionado por la tecnología.</h2>
         </div>
       </div>
     </div>
