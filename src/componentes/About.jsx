61bb3ab626d9bc4833cf4d6ce36ebb84f87cf1fc
kanazuko <eduardo.amata.cs@gmail.com>
2023-07-14 17:59:48 -0600

    Actualizando
    About y hero actualizados

--- src/componentes/About.jsx
+++ src/componentes/About.jsx
@@ -1,10 +1,31 @@
 import '../hojas-de-estilo/about.css'
+
 function About(){
     return(
-<section class="sobre-mi seccion-obscura">
-    <div class="contenedor">
-    <h2 class="seccion-titulo">Todo sobre GVA</h2>
-    <p class="seccion-texto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel fringilla enim. Curabitur sed nisi feugiat, suscipit nulla ut, eleifend metus. Sed tempor eu velit eget maximus. Suspendisse magna justo, tempor quis suscipit vel, dignissim id libero. Aliquam pulvinar nec erat sed dignissim. Quisque lobortis dignissim tempor. Nam volutpat sollicitudin lectus sit amet maximus. Vivamus bibendum ante eget massa tincidunt sodales. Nam id lacus a purus viverra tincidunt a eu leo. Proin facilisis pellentesque dapibus. Phasellus tempor purus a faucibus consectetur. Nunc id dolor id elit porttitor consequat. Aliquam cursus consectetur euismod. Nam fringilla elementum cursus.</p>
+<section className="sobre-mi seccion-obscura">
+    <div className="contenedor">
+    <h2 className="seccion-titulo">Un poco sobre mi...</h2>
+    <p className="seccion-texto">
+      Ingeniero en Ciencias Computacionales, egresado y titulado de la Universidad de Guadalajara,
+      que tiene gustos por la tecnología y le apasionan los retos, los acertijos, la resolución de problemas, 
+      el pensamiento crítico y el análisis.
+    </p>
+    <p className='seccion-texto'>El camino que me llevó a donde estoy data desde mi infancia, donde
+      siempre tuve un gusto por desarmar todo, desde juguetes hasta aparatos electrónicos, cajas fuertes,
+      bicicletas, bocinas, y todo aquello que se me cruzara, con el <strong>objetivo</strong> de conocer
+      cómo funcionan las cosas detrás de la fachada.
+    </p>
+    <p className='seccion-texto'>
+      Esta virtud, y mi pasión por la tecnología y la computación, me llevó a que a la edad de 12 años me
+      hiciera la pregunta que cambiaría mi vida.
+    </p>
+    <p className='seccion-titulo'>¿Cómo funciona una página web?</p>
+<a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+    <button type="button" className="btn btn-info">
+      Leer más sobre GVA
+      <i className="bi bi-arrow-right-circle-fill"></i>
+    </button>
+  </a>
     </div>
   </section>
     );
--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -1,8 +1,19 @@
 import '../hojas-de-estilo/hero.css'
 function Header(){
-    return(
-    <section className="hero align-items-stretch">
-    <div>Hola mundo? raro, no?
+    return(<section className="hero align-items-stretch">
+    <div className="hero-principal d-flex flex-column justify-content-center align-items-center">
+          <img className="hero-imagen-desarrollador" src={require("../imagenes/retratoTitulo.png")} width="100" alt="GVA Logo" />
+          <div className="row text-center align-items-center">
+            <div className="columna col-12 col-md-8">
+              <div className="hero-inferior">
+                <img className="hero-inferior-imagen img-fluid" src={require("../imagenes/4045240_a392_3.jpg")} alt="Monitor, laptop y logos" />
+              </div>
+            </div>
+          <div className="columna col-12 col-md-4">      
+          <h1>RAUL EDUARDO AVILA MATA</h1>
+          <h2>Desarrollador, ingeniero, gamer y apasionado por la tecnología.</h2>
+        </div>
+      </div>
     </div>
   </section>
   );
